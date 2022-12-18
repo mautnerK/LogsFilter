c196d723a4080f41e33aaf9762ac5520c6f2c1f4
mautnerK <mautner@net.hr>
2022-12-18 14:24:02 +0100

    Refactoring.

--- LogsFilter/Program.cs
+++ LogsFilter/Program.cs
@@ -3,57 +3,58 @@
 string[] lines = File.ReadAllLines("C:/Users/Karlo/Desktop/log20201104.txt");
 DateTime start;
 DateTime end;
+int startLocation;
+int endLocation;
 Console.WriteLine("1 for filter, 2 for time range");
-ConsoleKeyInfo action = Console.ReadKey();
-int actionSwitch = int.Parse(action.KeyChar.ToString());
-int charLocation;
-int charLocation2;
+ConsoleKeyInfo key = Console.ReadKey();
+int action = int.Parse(key.KeyChar.ToString());
 
-switch (actionSwitch)
+switch (action)
 {
     case 1:
         {
-            Console.WriteLine("\n Filter by : 1 for type, 2 for vault, 3 for modul");
-            ConsoleKeyInfo filter = Console.ReadKey();
-            int filterSwitch = int.Parse(filter.KeyChar.ToString());
+            Console.WriteLine("\n Filter by: 1 for type, 2 for vault, 3 for modul");
+            key = Console.ReadKey();
             Console.WriteLine("\n Input keyword");
+            int filter = int.Parse(key.KeyChar.ToString());
             string? keyword = Console.ReadLine();
-            switch (filterSwitch)
-            {  
-                    case 1: 
+
+            switch (filter)
+            {
+                case 1:
                     {
                         foreach (var line in lines)
                         {
-                            charLocation = line.IndexOf("[", StringComparison.Ordinal);
-                            charLocation2 = line.IndexOf("]", StringComparison.Ordinal);
-                            filteringString(line, charLocation, charLocation2, keyword.ToUpper());
+                            startLocation = line.IndexOf("[", StringComparison.Ordinal);
+                            endLocation = line.IndexOf("]", StringComparison.Ordinal);
+                            printFilteredLogs(line, startLocation, endLocation, keyword.ToUpper());
                         }
-                        return; 
+                        return;
                     }
-                    case 2: 
-                    {
-                    foreach (var line in lines)
-                        {
-                         charLocation = line.IndexOf("{", StringComparison.Ordinal);
-                         charLocation2 = line.IndexOf("}", StringComparison.Ordinal);
-                         filteringString(line, charLocation, charLocation2, keyword.ToUpper());
-                        }
-                        return; 
-                    }
-                    case 3: 
+                case 2:
                     {
                         foreach (var line in lines)
                         {
-                         charLocation = line.IndexOf("[", line.IndexOf("[")+1);
-                         charLocation2 = line.IndexOf("]", line.IndexOf("]") + 1);
-                         filteringString(line, charLocation, charLocation2, keyword);
+                            startLocation = line.IndexOf("{", StringComparison.Ordinal);
+                            endLocation = line.IndexOf("}", StringComparison.Ordinal);
+                            printFilteredLogs(line, startLocation, endLocation, keyword.ToUpper());
                         }
-                        return; 
+                        return;
                     }
-                }
+                case 3:
+                    {
+                        foreach (var line in lines)
+                        {
+                            startLocation = line.IndexOf("[", line.IndexOf("[") + 1);
+                            endLocation = line.IndexOf("]", line.IndexOf("]") + 1);
+                            printFilteredLogs(line, startLocation, endLocation, keyword);
+                        }
+                        return;
+                    }
+            }
             return;
         }
-    case 2: 
+    case 2:
         {
             Console.WriteLine("\n Input start date/time (Input type: year/month/day hour:minute:second.milisecond)");
             start = Convert.ToDateTime(Console.ReadLine());
@@ -61,16 +62,18 @@ switch (actionSwitch)
             end = Convert.ToDateTime(Console.ReadLine());
             foreach (var line in lines)
             {
-                charLocation = line.IndexOf(" +", StringComparison.Ordinal);
-                if (charLocation > 0)
+                startLocation = line.IndexOf(" +", StringComparison.Ordinal);
+                if (startLocation > 0)
                 {
-                    if (Convert.ToDateTime(line.Substring(0, charLocation)) >= start && Convert.ToDateTime(line.Substring(0, charLocation)) <= end) Console.WriteLine(line);
+                    if (Convert.ToDateTime(line.Substring(0, startLocation)) >= start && Convert.ToDateTime(line.Substring(0, startLocation)) <= end) Console.WriteLine(line);
                 }
             }
-                return;
+            return;
         }
-void filteringString(string line, int charLocation, int charLocation2, string keyword)
-        {   if (keyword == "" || keyword == null) Console.WriteLine("Incorrect input");
-            if (line.Substring(charLocation, charLocation2 - charLocation + 1).Contains(keyword)) Console.WriteLine(line);
+
+        void printFilteredLogs(string line, int startLocation, int endLocation, string keyword)
+        {
+            if (keyword == "" || keyword == null) Console.WriteLine("Incorrect input");
+            if (line.Substring(startLocation, endLocation - startLocation).Contains(keyword)) Console.WriteLine(line);
         }
 }
